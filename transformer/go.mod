8fadb1f4d85aa7c27f259092e878ad084daaa4cb
tpalinski <tpalinski@icloud.com>
2024-04-14 11:02:51 +0200

    [IO-14] Set up transformer with rmq

---
+++ transformer/go.mod
@@ -0,0 +1,10 @@
+module transformer
+
+go 1.18
+
+require (
+	github.com/rabbitmq/amqp091-go v1.9.0
+	github.com/sirupsen/logrus v1.9.3
+)
+
+require golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
---
+++ transformer/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"transformer/rabbit"
+	"transformer/utils"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+	log.Info("Starting image transformer")
+	err := rabbit.InitRabbitConnection(5, 10, nil);
+	utils.PanicOnError(err, "Something went horribly wrong")
+}
